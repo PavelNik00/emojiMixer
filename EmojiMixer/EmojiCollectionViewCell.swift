c3f1a7de0118a07f62c8b98f36bdaaf09a659f6d
PavelNik00 <gh9.111@yandex.ru>
2024-05-16 17:57:48 +0200

    Добавлена коллекция с эмоджи, настроено ее отображение в 2 столбца.

---
+++ EmojiMixer/EmojiCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiMixer
+//
+//  Created by Pavel Nikipelov on 16.05.2024.
+//
+
+import UIKit
+
+final class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    let label = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
--- EmojiMixer/ViewController.swift
+++ EmojiMixer/ViewController.swift
@@ -9,10 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let collectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        return collectionView
+    }()
+    
+    private let emoji = [ "🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍒", "🍓", "🫐", "🥝", "🍅", "🫒", "🥥", "🥑", "🍆", "🥔", "🥕", "🌽", "🌶️", "🫑", "🥒", "🥬", "🥦", "🧄", "🧅", "🍄"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.register(EmojiCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        view.addSubview(collectionView)
+        setupCollectionView()
+        
     }
-
+    
+    func setupCollectionView() {
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emoji.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? EmojiCollectionViewCell
+        
+        cell?.label.text = emoji[indexPath.row]
+        cell?.contentView.backgroundColor = .white
+        
+        return cell!
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width / 2, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
